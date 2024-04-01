fc1be68bda40a4690f323beefeafa789ea4cfe08
ilyapatis24 <ilyapatis24@mail.ru>
2024-04-01 20:35:15 +0300

    second mission is completed

---
+++ 02/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+template <class T>
+void print_container(const T& cont)
+{
+	for (auto it = cont.cbegin(); it != cont.cend(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+
+}
+
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Russian");
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+	return 0;
+}
